b0ea9d2a403526a09336bb3d01af998aa14475c9
isaias3silva <isaias3silva@gmail.com>
2021-11-11 18:24:40 -0300

    first commit

---
+++ src/main/java/com/mmlogistica/testsUnits/TestsUnitsApplication.java
@@ -0,0 +1,13 @@
+package com.mmlogistica.testsUnits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsUnitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsUnitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmlogistica/testsUnits/TestsUnitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmlogistica.testsUnits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsUnitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
